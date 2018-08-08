c9b64b1f11b0940f7e5793f15865e0b865f38bc5
Stewart ilondanga <stilondanga@gmail.com>
2018-08-08 10:36:30 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,11 @@ const COLORS = ['#333', '#7ECEFD'];
 
 const MAX_FOLLOWERS = 1500;
 
-/*const MY_TWITTER_USER = "rominamartinlib";
+const MY_TWITTER_USER = "rominamartinlib";
 const MY_CODEPEN_USER = "rominamartin";
 
 // Data
-const CODEPEN_BASE_URL = "https://cpv2api.com/profile/";
+/*const CODEPEN_BASE_URL = "https://cpv2api.com/profile/";
 const TWITTER_BASE_URL = "https://cors-anywhere.herokuapp.com/https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=";
 
 
