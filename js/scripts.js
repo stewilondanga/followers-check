97fc4b5138d6f97177fc045f0307e556b1963dc7
Stewart ilondanga <stilondanga@gmail.com>
2018-08-08 11:35:12 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ const GRAVITY = 0.3;
 const FRICTION = 0.5;
 const COLORS = ['#333', '#7ECEFD'];
 
-const MAX_FOLLOWERS = 1500;
+const MAX_FOLLOWERS = 1500000;
 
 const MY_TWITTER_USER = "stew_ilondanga";
 const MY_CODEPEN_USER = "stew_ilondanga";
